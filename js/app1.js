6df4b1ee56351cee972d5f4c52eb1e063ab3dfd4
Juan <og.jjoa@gmail.com>
2021-08-13 16:33:32 -0300

    Horizontal bar problem fixed

---
+++ js/app1.js
@@ -0,0 +1,20 @@
+const link1 = document.querySelector("#link1");
+const link2 = document.querySelector("#link2");
+const link3 = document.querySelector("#link3");
+
+//Events
+// link1.addEventListener("mouseenter", () => {
+//   const list1 = document.querySelector("#list1");
+//   const image1 = document.querySelector("image1");
+//   list1.classList.add("hide");
+//   image1.classList.remove("hide");
+// });
+// link2.addEventListener("mouseover", () => {
+//   const list2 = document.querySelector("#list2");
+//   const image2 = document.querySelector("image2");
+//   list2.classList.add("hide");
+//   image2.classList.remove("hide");
+// });
+// link3.addEventListener("mouseover", showCoin(3));
+
+//Functions
